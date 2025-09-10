fc141c7cae9c0dada4cbd6106462a38849288301
QuangDev <ng.quang.coder.new@gmail.com>
2025-09-10 11:09:31 +0700

    Tạo kết nối với mySQL

--- nest-learn/src/app.module.ts
+++ nest-learn/src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [CatModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_api_v1',
+      entities: [],
+      synchronize: true,
+    }),
+    CatModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
