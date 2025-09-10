6ab141269bde6f6d57be5ddd0a1884bf57ea111f
QuangDev <ng.quang.coder.new@gmail.com>
2025-09-10 11:20:58 +0700

    Tạo Entity theo bảng trong mySQL

--- nest-learn/src/cat/entities/cat.entity.ts
+++ nest-learn/src/cat/entities/cat.entity.ts
@@ -1,1 +1,16 @@
-export class Cat {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('cats') // tên bảng trong DB, mặc định sẽ là 'cat' nếu không truyền
+export class Cat {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  breed: string;
+}
